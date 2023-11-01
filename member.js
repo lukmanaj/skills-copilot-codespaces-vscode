e9733e4ece858fb49a686d9bdef8f84d0721ca7b
Lukman Aliyu Jibril <aliyulukman94@gmail.com>
2023-11-01 16:25:18 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberValue = member.options[member.selectedIndex].value;
+    document.getElementById("memberText").innerHTML = memberText;
+    document.getElementById("memberValue").innerHTML = memberValue;
+
+}
